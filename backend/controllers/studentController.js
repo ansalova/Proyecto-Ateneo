434af684c8aa8645fee42c27c1088c4814817d57
Andres <saintlvoficial@gmail.com>
2026-04-20 15:49:37 -0500

    arreglo en notas

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -3,10 +3,17 @@ import { getGradesForStudent } from "../models/Grade.js";
 // @desc    Obtener mis calificaciones
 // @route   GET /api/student/my-grades
 // @access  Private (Student)
+
 export const getMyGrades = async (req, res) => {
   try {
     const grades = await getGradesForStudent(req.user.id);
-    res.json(grades);
+
+    res.json({
+      name: req.user.name,
+      email: req.user.email,
+      grades
+    });
+
   } catch (error) {
     if (error.message === "DB_NOT_CONFIGURED") {
       return res.status(500).json({ msg: "Base de datos no configurada." });
